7b60d0324e5b4d342800937c9a726b6772b73b7a
sathishkumar NATARAJ <32127136+sathishkumarece@users.noreply.github.com>
2020-11-11 21:07:18 +0530

    Feat: Introduce illustrator image

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Container, Row, Col} from 'react-bootstrap'
 
 import bg_img from '../assest/img/bookshelf.jpg'
+import lib_img from '../assest/img/library.png'
 import './Home.css'
 
 function Home() {
@@ -13,6 +14,7 @@ function Home() {
                         <div className="title">
                             Welcome to the world's largest collection of books
                         </div>
+                        <img src={lib_img} alt="Library" className="lib-img"/>
                     </Col>
                     <Col xs={12} sm={12} md={6} lg={6}>
             
